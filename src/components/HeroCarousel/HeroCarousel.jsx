c638253e4f51cbfacf80506f44f3bae132a09a33
geekySapien <escaping.blurred.reality@gmail.com>
2021-08-09 18:04:43 +0530

    Added the Arrow Component

--- src/components/HeroCarousel/HeroCarousel.jsx
+++ src/components/HeroCarousel/HeroCarousel.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
 import HeroSlider from "react-slick";
 
+//component
+
+import { NextArrow, PrevArrow } from './Arrows.component';
+
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const HeroCarousel = () => {
     const settingsLg={
-        dots:true,
         arrows:true,
         autoplay:true,
         centerMode:true,
         centerPadding:"300px",
         infinite: true,
         slidesToShow:1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        nextArrow: <NextArrow/>,
+        prevArrow: <PrevArrow/>
     };
     const settings={
     arrows:true, 
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    nextArrow: <NextArrow/>,
+    prevArrow: <PrevArrow/>
     };
     const images=["https://images.unsplash.com/photo-1593642532744-d377ab507dc8?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", 
     "https://images.unsplash.com/photo-1628417632477-5919c98218d5?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
@@ -30,7 +37,7 @@ const HeroCarousel = () => {
     "https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0OXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"];
     return (
         <>
-            <div className="lg:hidden overflow-x-hidden">
+            <div className="lg:hidden ">
                 <HeroSlider {...settings}>
                     {
                         images.map((image)=>(
