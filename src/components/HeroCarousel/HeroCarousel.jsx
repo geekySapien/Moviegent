26b67306ca8ebbed0b202ef5188a4426d27069af
geekySapien <escaping.blurred.reality@gmail.com>
2021-08-09 14:30:26 +0530

    Responsive Carousel component

--- src/components/HeroCarousel/HeroCarousel.jsx
+++ src/components/HeroCarousel/HeroCarousel.jsx
@@ -6,11 +6,17 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const HeroCarousel = () => {
+    const settingsLg={
+        arrows:true,
+        autoplay:true,
+        centerMode:true,
+        centerPadding:"300px",
+        infinite: true,
+        slidesToShow:1,
+        slidesToScroll: 1
+    };
     const settings={
     arrows:true, 
-    dots: true,
-    centerMode:true,
-    centerPadding:"300px",
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -22,18 +28,32 @@ const HeroCarousel = () => {
     "https://images.unsplash.com/photo-1627662056598-dcfc5000f769?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0OXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"];
     return (
-        <div>
-            <HeroSlider {...settings}>
-                {
-                    images.map((image)=>(
-                        <div className="h-80 w-20">
-                            <img src={`${image}`} alt="testing" className="h-full w-full" />
-                        </div>
-                    ))
-                }
-            </HeroSlider>
-        </div>
+        <>
+            <div className="lg:hidden">
+                <HeroSlider {...settings}>
+                    {
+                        images.map((image)=>(
+                            <div className="h-44 w-full py-3 md:h-64">
+                                <img src={image} alt="testing" className="h-full w-full" />
+                            </div>
+                        ))
+                    }
+                </HeroSlider>
+            </div>
+            <div className="hidden lg:block">
+                    <HeroSlider {...settingsLg}>
+                        {
+                            images.map((image)=>(
+                                <div className="w-20 h-80 px-2 py-3">
+                                    <img src={image} alt="testing" className="w-full h-full" />
+                                </div>
+                            ))
+                        }
+                    </HeroSlider>
+            </div>
+        </>
     )
+
 }
 
 export default HeroCarousel;
