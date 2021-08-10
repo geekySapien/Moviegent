7753a72fc781db9f90f647b6e5b758b0a67bfadf
geekySapien <escaping.blurred.reality@gmail.com>
2021-08-10 18:31:48 +0530

    Added the premiere component

--- src/Pages/Home.page.jsx
+++ src/Pages/Home.page.jsx
@@ -9,12 +9,25 @@ const HomePage=()=>{
     return (
         <>
 
+        <div className="flex flex-col gap-10">
         <div className="container mx-auto px-4">
-        <h1 className="text-3xl font-bold text-gray-800 my-3">The Best of Entertainment</h1>
+        <h1 className="text-3xl font-bold text-gray-800 my-3 ">The Best of Entertainment</h1>
         <EntertainmentCardSlider/>
-        <Premiere/>
-        
         </div>
+        <div className="bg-premiere-800 py-12 ">
+        <div className="container mx-auto px-4 flex flex-col gap-3" >
+            <div className="hidden md:flex ">
+                <div className="w-full h-24">
+                    <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png" alt="Premiere" className="w-full h-full"/>
+                    
+                </div>
+                
+            </div>
+            <Premiere/>
+        </div>
+        </div>    
+        </div>
+
         </>
     )
 }
--- src/components/Premiere/Premiere.component.jsx
+++ src/components/Premiere/Premiere.component.jsx
@@ -69,13 +69,23 @@ const Premiere = () => {
             title:"Radioactive",
             subtitle:"English"
         },
+        {
+            src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302293-savbwcehey-portrait.jpg",
+            alt:"Radioactive",
+            title:"Radioactive",
+            subtitle:"English"
+        }
     ]
     return (
         <>
+        <div className="flex flex-col items-start">
+        <h3 className="text-xl text-white font-bold">Premieres</h3>
+        <p className="text-sm text-white">Brand New Releases Every Friday</p>
+        </div>
          <Slider {...settings}>
              {
                  images.map((image)=>(
-                     <Poster {...image}/>
+                     <Poster {...image} isDark/>
     ))
              }
          </Slider>   
