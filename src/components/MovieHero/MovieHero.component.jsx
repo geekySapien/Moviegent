801e8c51d07fa708e3e65d5d4411ae3b9237dd22
geekySapien <escaping.blurred.reality@gmail.com>
2021-08-18 13:34:14 +0530

    Added the MovieHero Component

--- src/components/MovieHero/MovieHero.component.jsx
+++ src/components/MovieHero/MovieHero.component.jsx
@@ -13,12 +13,12 @@ const MovieHero = () => {
                     <img src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg" alt="Poster" className="w-full h-full"/>
                 </div>
               <div className="relative hidden w-full lg:block" style={{height:"30rem"}}>
-                    <div className="absolute w-full h-full z-10" style={{backgroundImage: " linear-gradient(90deg, rgb(34, 34, 34) 24.97%, rgb(34, 34, 34) 38.3%, rgba(34, 34, 34, 0.04) 97.47%, rgb(34, 34, 34) 100%)"}}/>
+              <div className="absolute w-full h-full z-10" style={{backgroundImage: " linear-gradient(90deg, rgb(34, 34, 34) 24.97%, rgb(34, 34, 34) 38.3%, rgba(34, 34, 34, 0.04) 97.47%, rgb(34, 34, 34) 100%)"}}/>
+              <div className=" absolute w-64 h-96 z-30 left-24 top-10 ">
+                  <img src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg" alt="Poster" className="h-full w-full rounded-xl"/>
+              </div>   
 
-                    
-
-
-                    <img src="https://in.bmscdn.com/iedb/movies/images/extra/horizontal_no_logo/mobile/listing/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg" className="w-full h-full"/>
+              <img src="https://in.bmscdn.com/iedb/movies/images/extra/horizontal_no_logo/mobile/listing/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg" className="w-full h-full"/>
                     
                 </div>
             </div>
