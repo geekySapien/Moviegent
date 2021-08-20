a094c9cfed30a70c6ef1a350aa942901d12a4144
geekySapien <escaping.blurred.reality@gmail.com>
2021-08-20 13:51:18 +0530

    Added the movie heo component

--- src/components/MovieHero/MovieHero.component.jsx
+++ src/components/MovieHero/MovieHero.component.jsx
@@ -10,15 +10,21 @@ const MovieHero = () => {
             <div>
                 <div className="relative md:hidden" style={{height: "calc(180vw)"}}>
                     <div className="w-full h-56 bg-opacity-80 absolute bg-black z-10 bottom-0"/>
+                    <div className="absolute z-20 bottom-4 left-4">
+                        <MovieInfo/>
+                    </div>
                     <img src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg" alt="Poster" className="w-full h-full"/>
                 </div>
                 <div className="relative hidden md:block lg:hidden" style={{height: "calc(100vw)"}}>
                     <div className="w-full h-56 bg-opacity-80 absolute bg-black z-10 bottom-0"/>
+                    <div className="absolute z-20 bottom-4 ">
+                        <MovieInfo/>
+                    </div>
                     <img src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg" alt="Poster" className="w-full h-full"/>
                 </div>
               <div className="relative hidden w-full lg:block" style={{height:"30rem"}}>
               <div className="absolute w-full h-full z-10" style={{backgroundImage: " linear-gradient(90deg, rgb(34, 34, 34) 24.97%, rgb(34, 34, 34) 38.3%, rgba(34, 34, 34, 0.04) 97.47%, rgb(34, 34, 34) 100%)"}}/>
-              <div className="absolute z-30 left-24 top-10 flex items-center">
+              <div className="absolute z-30 left-24 top-10 flex items-center gap-10 ">
                 <div className=" w-64 h-96  ">
                   <img src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg" alt="Poster" className="h-full w-full rounded-xl"/>
                 </div>   
--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -3,24 +3,26 @@ import React from 'react'
 const MovieInfo = () => {
     return (
         <>
-            <div>
-                <div className="flex items-center gap-3">
+            <div className="flex flex-col gap-3  lg:gap-8"> 
+                <div className="flex items-center gap-3 md:px-4">
                     <div className="w-40 h-8">
                         <img src="https://in.bmscdn.com/moviemode/tvod/premiere-tag.png" alt="Premiere"
                           className="w-full h-full"/>
                     </div>
-                    <span className="bg-bms-700 p-1 text-white rounded-md">Streaming Now</span>
+                    <span className="bg-bms-800 p-1 text-white rounded-md">Streaming Now</span>
                 </div>
-                <h1 className="text-white lg:text-5xl font-bold">Justice League</h1>
-                <div className="font-light flex flex-col gap-2">
-                    <div className="text-white text-sm"> 4K &bull; English &bull; 
-                        <span className="text-red-500 font-bold">Subtitles(1)</span> 
+                <h1 className="hidden lg:block text-white lg:text-5xl font-bold">Justice League</h1>
+                <div className="flex flex-col-reverse lg:flex-col gap-3 lg:gap-5">
+                    <div className="font-light flex flex-col gap-2 md:px-4">
+                        <div className="text-white text-sm"> 4K &bull; English &bull; 
+                            <span className="text-red-500 font-bold">Subtitles(1)</span> 
+                        </div>
+                        <div className="text-white text-sm ">1hr 53min &bull; Action, Thriller, Adventure</div>
                     </div>
-                    <div className="text-white text-sm ">1hr 53min &bull; Action, Thriller, Adventure</div>
-                </div>
-                <div>
-                   <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">Rent ₹149</button> 
-                   <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">Buy ₹149 </button>
+                    <div className="flex items-center gap-3 md:w-screen lg:w-full md:px-4">
+                        <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">Rent ₹149</button> 
+                        <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">Buy ₹149 </button>
+                    </div>
                 </div>
             </div>
         </>
