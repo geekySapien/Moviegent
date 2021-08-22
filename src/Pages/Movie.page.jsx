87c3c4e2a036b6b0dee72da46d73c72d7438c5e9
geekySapien <escaping.blurred.reality@gmail.com>
2021-08-22 11:04:30 +0530

    added offers

--- src/Pages/Movie.page.jsx
+++ src/Pages/Movie.page.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import {FaCcVisa,FaCcApplePay} from 'react-icons/fa'
+
 //component
 import MovieHero from '../components/MovieHero/MovieHero.component'
 
@@ -16,13 +18,21 @@ const Movie = () => {
              <div className="my-8">
                 <hr/>
              </div>  
-             <div className="my-8">
-                 <h1 className="text-gray-800 text-lg font-bold">
-                     Applicable Offers </h1>
-                 <div className="flex item-start gap-2">
-                     
-                 </div>
-             </div>
+            <div className="my-8">
+                <h1 className="text-gray-800 text-lg font-bold">
+                     Applicable Offers 
+                </h1>
+            
+                <div className="flex items-start  bg-yellow-200 gap-4 p-3 border border-yellow-400 border-dashed">
+                    <div className="w-8 h-8">
+                        <FaCcVisa className="w-full h-full"/>
+                    </div>
+                    <div className="flex flex-col items-start gap-1">
+                        <h3 className="text-gray-700 text-xl font-bold">Visa Streamming Offer</h3>
+                        <p className="text-gray-600">Get 50% off up to INR 150 on all Visa cards* on BookMyShow Stream.</p>
+                    </div> 
+                </div>
+            </div>
         </div>
         </>
     )
