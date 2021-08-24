aa178f10ab359bfbcf124cae4d6397dadea7ccb5
geekySapien <escaping.blurred.reality@gmail.com>
2021-08-24 21:50:15 +0530

    added the cast and cre component

--- src/Pages/Movie.page.jsx
+++ src/Pages/Movie.page.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 import {FaCcVisa,FaCcApplePay} from 'react-icons/fa'
 
+
 //component
 import MovieHero from '../components/MovieHero/MovieHero.component'
+import Cast from '../components/Cast/Cast.component'
 
 const Movie = () => {
     return (
@@ -11,7 +13,7 @@ const Movie = () => {
         <MovieHero/>
         <div className="my-12 container lg:ml-20 px-4 lg:w-2/3">
              <div className="flex flex-col items-start gap-3 ">
-                 <h1 className="text-gray-800 text-2xl font-bold">About the movie</h1>
+                 <h2 className="text-gray-800 text-2xl font-bold">About the movie</h2>
                  <p className="">Bruce Wayne and Diana Prince try to bring the metahumans of Earth together 
                  after the death of Clark Kent. Meanwhile, Darkseid sends Steppenwolf to Earth with an army to subjugate humans.</p>
              </div> 
@@ -44,6 +46,18 @@ const Movie = () => {
                     </div>
                 </div>
             </div>
+            <div className="my-8">
+                <h2 className="text-xl font-bold text-gray-800">Cast and Crew</h2>
+            </div>
+            <div className="flex flex-wrap gap-4">
+                <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/ben-affleck-292-12-09-2017-05-12-16.jpg" castName="Ben Alfeck" role="Batman"/>
+                
+                <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/henry-cavill-23964-04-05-2020-04-25-14.jpg" castName="Henry Cavil" role="Superman"/>
+                 
+                <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/gal-gadot-11088-17-10-2017-11-45-36.jpg" castName="Gal Gadot" role="Wonder Woman"/>
+                
+            </div>
+
         </div>
         </>
     )
