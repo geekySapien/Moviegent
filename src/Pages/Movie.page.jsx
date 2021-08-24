8c77eb081ed608a5e72091db20a7a235e5a5d01b
geekySapien <escaping.blurred.reality@gmail.com>
2021-08-24 12:31:14 +0530

    added the discount component

--- src/Pages/Movie.page.jsx
+++ src/Pages/Movie.page.jsx
@@ -18,19 +18,30 @@ const Movie = () => {
              <div className="my-8">
                 <hr/>
              </div>  
-            <div className="my-8">
-                <h1 className="text-gray-800 text-lg font-bold">
+            <div className="my-8 flex flex-col gap-3">
+                <h2 className="text-gray-800 text-xl font-bold">
                      Applicable Offers 
-                </h1>
+                </h2>
             
-                <div className="flex items-start  bg-yellow-200 gap-4 p-3 border border-yellow-400 border-dashed">
-                    <div className="w-8 h-8">
-                        <FaCcVisa className="w-full h-full"/>
+                <div className="flex flex-col gap-3 lg:flex-row">
+                    <div className="flex items-start  bg-yellow-100 gap-4 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+                        <div className="w-8 h-8">
+                            <FaCcVisa className="w-full h-full"/>
+                        </div>
+                        <div className="flex flex-col items-start gap-1">
+                            <h3 className="text-gray-700 text-xl font-bold">Visa Streamming Offer</h3>
+                            <p className="text-gray-600">Get 50% off up to INR 150 on all Visa cards* on BookMyShow Stream.</p>
+                        </div> 
                     </div>
-                    <div className="flex flex-col items-start gap-1">
-                        <h3 className="text-gray-700 text-xl font-bold">Visa Streamming Offer</h3>
-                        <p className="text-gray-600">Get 50% off up to INR 150 on all Visa cards* on BookMyShow Stream.</p>
-                    </div> 
+                    <div className="flex items-start  bg-yellow-100 gap-4 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+                        <div className="w-10 h-10">
+                            <FaCcApplePay className="w-full h-full"/>
+                        </div>
+                        <div className="flex flex-col items-start gap-1">
+                            <h3 className="text-gray-700 text-xl font-bold">Filmy Pass</h3>
+                            <p className="text-gray-600">Get Rs.75* off on 3 movies you buy/rent on Stream. Buy Filmy Pass @Rs.99</p>
+                        </div> 
+                    </div>
                 </div>
             </div>
         </div>
