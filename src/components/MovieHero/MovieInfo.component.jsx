0f8aff38f4f986111986efeed5b7fa0b364fbdf3
geekySapien <escaping.blurred.reality@gmail.com>
2021-09-04 14:41:51 +0530

    added Razorpay gateway

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,13 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MovieContext } from "../../context/movie.context";
+
+//components
+
+import PaymentModal from "../PaymentModal/Payment.componet";
+
 const MovieInfo = () => {
   const { movie } = useContext(MovieContext);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [price, setPrice] = useState(0);
   //optional chaining
   const genres = movie.genres?.map(({ name }) => name).join(", ");
 
+  const rentMovies = () => {
+    setIsOpen(true);
+    setPrice(149);
+  };
+
+  const buyMovies = () => {
+    setIsOpen(true);
+    setPrice(599);
+  };
+
   return (
     <>
+      <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price} />
       <div className="flex flex-col gap-3  lg:gap-8">
         <div className="flex items-center gap-3 md:px-4">
           <div className="w-40 h-8">
@@ -31,15 +48,21 @@ const MovieInfo = () => {
               <span className="text-red-500 font-bold"> Subtitles(1)</span>
             </div>
             <div className="text-white text-sm ">
-              {(movie.runtime/60).toFixed(2)} h &bull; {genres}
+              {(movie.runtime / 60).toFixed(2)} h &bull; {genres}
             </div>
           </div>
           <div className="flex items-center gap-3 md:w-screen lg:w-full md:px-4">
-            <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
+            <button
+              onClick={rentMovies}
+              className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg"
+            >
               Rent ₹149
             </button>
-            <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
-              Buy ₹149
+            <button
+              onClick={buyMovies}
+              className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg"
+            >
+              Buy ₹599
             </button>
           </div>
         </div>
